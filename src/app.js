6c4276b429e8561b5038f782e29552b1e2fc4841
Flavio Rocha <fmrocha@gmail.com>
2020-05-20 18:31:09 -0700

    test 9 passed, 9 total

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {
+    url,
+    title,
+    techs,
+    likes = 0,
+  } = request.body;
+
+  const id =  uuid();
+
+  repositories.push({ id, url, title, techs, likes }); 
+
+  return response.json({ id, url, title, techs, likes } );
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {
+    url,
+    title,
+    techs,
+    likes,
+  } = request.body;
+
+  const { id } = request.params;
+
+  if (likes) { return response.json({ likes: 0 })}
+
+  const repositoryIndex = (repositories) => repositories.id === id;
+
+  if (repositories.findIndex(repositoryIndex) < 0) { 
+    return response.status(400).json('Bad Request')
+  };
+
+  repositories[repositories.findIndex(repositoryIndex)] = { id, url, title, techs };
+
+  return response.json({ id, url, title, techs });
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = (repositories) => repositories.id === id;
+
+  if (repositories.findIndex(repositoryIndex) < 0) { 
+    return response.status(400).json('Bad Request')
+  };
+
+  repositories.splice(repositories.findIndex(repositoryIndex), 1);
+
+  return response.status(204).json(repositories);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = (repositories) => repositories.id === id;
+
+  if (repositories.findIndex(repositoryIndex) < 0) { 
+    return response.status(400).json('Bad Request')
+  };
+
+  repositories[repositories.findIndex(repositoryIndex)].likes++; 
+
+  return response
+    .status(200)
+    .json({ likes: repositories[repositories.findIndex(repositoryIndex)].likes });
 });
 
 module.exports = app;
